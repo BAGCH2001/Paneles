b97afbd39dc188f9b2be6bd321d06a17136d53bc
BAGCH2001 <82058208+BAGCH2001@users.noreply.github.com>
2023-01-26 22:03:50 -0600

    panel con mui
    diseño

--- src/Auth/PanelAn.jsx
+++ src/Auth/PanelAn.jsx
@@ -1,100 +1,188 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
 
 export const PanelAn = () => {
-    return (
-        <div class="flex font-sans">
-            <div class="flex-none w-64 h-screen relative border-2 border-r-gray-300 border-l-none">
-                <div class="flex-1 flex-col  ...">
-                    <div ><img className="w-auto h-auto" src="https://i.ibb.co/sK4fDQm/image-5.png" alt="" /></div>
-                    <div class="rounded-full ...">
-                        <img src="" alt="" />
-                    </div>
-                    <hr />
-                    <div className='bg-[#]'>
-                        <p className='font-bold text-center'>Usuario</p>
-                        <p className='underline underline-offset-1 text-center'>correo@gmail.com</p>
-                        <p className='font-bold text-center'>Publicaciones</p>
-                    </div>
-                    <hr />
-                    {/*<div className="flex mx-auto">
-                        <Link to="../inicio" className="btn btn-ghost normal-case text-  text-[#FFFFFF]" >inicio</Link>
-                    </div> si jala pero no sabemos como cargarlo en otro lado
-                    */}
-                    <div class="grid grid-cols-1 divide-y text-center border-gray-400">
-                    <div className="flex mx-auto">
-                        <Link to="../inicio" className="btn btn-ghost normal-case text-  text-[#FFFFFF]" >inicio</Link>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <form class="flex-auto p-6">
-                <div class="flex flex-wrap">
-                    <h1 class="flex-auto text-lg font-semibold text-slate-900">
-                        Classic Utility Jacket
-                    </h1>
-                    <div class="text-lg font-semibold text-slate-500">
-                        $110.00
-                    </div>
-                    <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-                        In stock
-                    </div>
-                </div>
-                <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-                    <div class="space-x-2 flex text-sm">
-                        <label>
-                            <input class="sr-only peer" name="size" type="radio" value="xs" checked />
-                            <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-                                XS
-                            </div>
-                        </label>
-                        <label>
-                            <input class="sr-only peer" name="size" type="radio" value="s" />
-                            <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-                                S
-                            </div>
-                        </label>
-                        <label>
-                            <input class="sr-only peer" name="size" type="radio" value="m" />
-                            <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-                                M
-                            </div>
-                        </label>
-                        <label>
-                            <input class="sr-only peer" name="size" type="radio" value="l" />
-                            <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-                                L
-                            </div>
-                        </label>
-                        <label>
-                            <input class="sr-only peer" name="size" type="radio" value="xl" />
-                            <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-                                XL
-                            </div>
-                        </label>
-                    </div>
-                </div>
-                <div class="flex space-x-4 mb-6 text-sm font-medium">
-                    <div class="flex-auto flex space-x-4">
-                        <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-                            Buy now
-                        </button>
-                        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
-                            Add to bag
-                        </button>
-                    </div>
-                    <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
-                        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-                        </svg>
-                    </button>
-                </div>
-                <p class="text-sm text-slate-700">
-                    Free shipping on all continental US orders.
-                </p>
-            </form>
-        </div>
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    )
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" severity="warning" color="" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Xidoo Rutas
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+       <Box>
+
+       </Box>
+      </Box>
+    </Box>
+  );
 }
--- src/Router/Approuter.jsx
+++ src/Router/Approuter.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import {Inicio} from "../Public/Inicio"
 import { PanelAn } from "../Auth/PanelAn"
-
 export const Approuter = () => {
   return (
     <>
