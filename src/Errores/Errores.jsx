5a19a484a70edb85c2696c75acde03bcd6e05e54
Furry <612010285@utsalamanca.edu.mx>
2023-02-01 13:06:51 -0600

    todo centrado alv

--- src/Errores/Errores.jsx
+++ src/Errores/Errores.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import errorImage from '../img/404.svg'
 const Errores = () => {
   return (
-    <div className="h-screen bg-gray-200">
-      <div className="flex items-center justify-center h-full">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <div className="col-span-3 lg:col-span-1 text-center lg:text-center">
-            <img src={errorImage} alt="404 Error" className="w-48 mb-4" />
+   
+    <div className="h-screen bg-[#333333]">
+      <div className="flex items-center h-full">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 m-40">
+          <div className="col-span-1 lg:col-span-1 text-center ">
+            <img src={errorImage} alt="404 Error" className="w-65 mb-4" />
           </div>
-          <div className="col-span-3 lg:col-span-2 text-center lg:text-left">
-            <h1 className="text-5xl font-bold text-gray-700">404</h1>
-            <p className="text-gray-600">No hay ruta pa' allá mi chavo</p>
+
+          
+
+          <div className="m-auto col-span-2 text-center justify-center">
+            <h1 className="text-8xl font-bold text-white">404</h1>
+            <p className="text-white">No hay ruta pa' allá mi chavo</p>
           </div>
         </div>
       </div>
